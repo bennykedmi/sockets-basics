9b48f7e2ae72ad9db10654ddefbcc5bd2b831d73
Benny kedmi <bennykedmi@gmail.com>
2017-03-05 08:06:31 +0200

    Add support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,28 @@
 var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
-
+var socket = io();
 
 console.log(name + ' wants to join ' + room);
-var socket = io();
+
+// Update h1 tag
+jQuery('.room-title').text(room);
 
 socket.on('connect', function () {
 	console.log('Conncted to socket.io server!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
 
 socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
 	var $message = jQuery('.messages');
+
 	console.log('New message:');
 	console.log(message.text);
 
-	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') +'</strong></p>');
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
 	$message.append('<p>' + message.text + '</p>');
 });
 
@@ -28,7 +35,7 @@ $form.on('submit', function (event) {
 	var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
-		name:name,
+		name: name,
 		text: $message.val()
 	});
 
